2ae6e2e889331bba92bc63f943f9c3ba443a4d36
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-03-21 20:05:45 +0000

    primera version menu desplegable

---
+++ JS_Files/help-script.js
@@ -0,0 +1,22 @@
+let toggles = document.getElementsByClassName('menu-nav');
+let contentDiv = document.getElementsByClassName('parrafo');
+
+for(let i=0; i<toggles.length; i++){
+    toggles[i].addEventListener('click', ()=>{
+        if( parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
+            contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+            toggles[i].style.color = "#0084e9";
+        }
+        else{
+            contentDiv[i].style.height = "0px";
+            toggles[i].style.color = "#111130";
+        }
+
+        for(let j=0; j<contentDiv.length; j++){
+            if(j!==i){
+                contentDiv[j].style.height = "0px";
+                toggles[j].style.color = "#111130";
+            }
+        }
+    });
+}
