5e8e541ac72b9750c7eb4148dc9bc3985b74db09
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2022-03-21 19:07:20 +0000

    Añadida funcionalidad de mostrar contraseña al popup de login

--- JS_Files/login_script.js
+++ JS_Files/login_script.js
@@ -1,7 +1,10 @@
 var login_open = document.getElementById('user_profile'),
     login_overlay = document.getElementById('login_popup_overlay'),
     login_popup = document.getElementById('login_popup'),
-    login_close = document.getElementById('btn-cerrar-login_popup');
+    login_close = document.getElementById('btn-cerrar-login_popup'),
+    login_vision = document.getElementById('vision'),
+    login_sin_vision = document.getElementById('sin_vision'),
+    login_password = document.getElementById('login_password');
 
 login_open.addEventListener('click', function () {
     login_overlay.classList.add('login_active');
@@ -12,4 +15,30 @@ login_close.addEventListener('click', function (e) {
     e.preventDefault();
     login_overlay.classList.remove('login_active');
     login_popup.classList.remove('login_active');
+});
+
+login_sin_vision.addEventListener('click', function (){
+    login_sin_vision.classList.remove('activation');
+    login_vision.classList.add('activation');
+});
+
+login_vision.addEventListener('click', function (){
+    login_vision.classList.remove('activation');
+    login_sin_vision.classList.add('activation');
+});
+
+login_vision.addEventListener('click', function() {
+    if (login_vision.classList.contains('activation')) {
+        login_password.type = "text";
+    } else {
+        login_password.type = "password";
+    }
+});
+
+login_sin_vision.addEventListener('click', function() {
+    if (login_vision.classList.contains('activation')) {
+        login_password.type = "text";
+    } else {
+        login_password.type = "password";
+    }
 });
