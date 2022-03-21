fc7fab5ba4a87697ffefa7add4c33be864897def
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2022-03-21 17:47:00 +0000

    Añadido popup de login

---
+++ JS_Files/login_script.js
@@ -0,0 +1,15 @@
+var login_open = document.getElementById('user_profile'),
+    login_overlay = document.getElementById('login_popup_overlay'),
+    login_popup = document.getElementById('logging_popup'),
+    login_close = document.getElementById('btn-cerrar-login_popup');
+
+login_open.addEventListener('click', function () {
+    login_overlay.classList.add('login_active');
+    login_popup.classList.add('login_active');
+});
+
+login_close.addEventListener('click', function (e) {
+    e.preventDefault();
+    login_overlay.classList.remove('login_active');
+    login_popup.classList.remove('login_active');
+});
