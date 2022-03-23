fe2ab433bb4fd15a510c9694aec2b764a2068053
Sebastián Fernández <sebasfernandezgar@gmail.com>
2022-03-23 15:08:42 +0000

    Load Cart items from Json using Jquery + improvements on Responsive design using Media Query

---
+++ mispruebas/main.js
@@ -0,0 +1,17 @@
+//In your script
+
+$("#my_image").attr("src","/IMGS/purchase/security.png");
+
+
+const image_input = document.querySelector("#image_input");
+var uploaded_image;
+
+image_input.addEventListener("change", function() {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        uploaded_image = reader.result;
+        document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
+    });
+    reader.readAsDataURL(this.files[0]);
+});
+
