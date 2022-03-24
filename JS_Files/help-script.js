c13ebb2990ac0076e1c5d20a4683a2841c55faac
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-03-24 17:42:23 +0000

    primer diseño adaptable a movil

--- JS_Files/help-script.js
+++ JS_Files/help-script.js
@@ -5,17 +5,17 @@ for(let i=0; i<toggles.length; i++){
     toggles[i].addEventListener('click', ()=>{
         if( parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
             contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
-            toggles[i].style.color = "#0084e9";
+            toggles[i].style.color = "#FFA16D";
         }
         else{
             contentDiv[i].style.height = "0px";
-            toggles[i].style.color = "#111130";
+            toggles[i].style.color = "#FFA16D";
         }
 
         for(let j=0; j<contentDiv.length; j++){
             if(j!==i){
                 contentDiv[j].style.height = "0px";
-                toggles[j].style.color = "#111130";
+                toggles[j].style.color = "#FFA16D";
             }
         }
     });
