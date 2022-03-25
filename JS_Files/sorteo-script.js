0a0f1595c7fe6ec30886b1cb8ff453551de6828d
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-03-25 14:14:03 +0000

    idea feliz: [FUNCIONA] implementacion json en la pagina de sorteos

--- JS_Files/sorteo-script.js
+++ JS_Files/sorteo-script.js
@@ -1,17 +1,15 @@
-$.getJSON("/JSON_Files/sorteo-script.json", function(data) {
-    $.each(data.img-sorteos, function(key, value) {
-        let paco;
-        let c=1;
+$.getJSON("/JSON_Files/imgSorteo.json", function(data) {
+    $.each(data.imgLottery, function(key, value) {
+        let item;
 
-        paco= '<section class="sorteo'+c+'">' +
-                '<label class="txt-sorteo'+c+'">' + value.title + '</label>' +
-                '<img class="img-sorteo1" src="' + value.img + '" alt="sorteo 1"><br>' +
-                '<button class="unir-sorteo'+c+'">' +
+        item = '<section class="sorteo'+value.id+'">' +
+                '<label class="txt-sorteo'+value.id+'">' + value.title + '</label>' +
+                '<img class="img-sorteo'+value.id+'" src="' + value.img + '" alt="sorteo '+value.id+'"><br>' +
+                '<button class="unir-sorteo'+value.id+'">' +
                     '<a href="entrar_sorteo.html" class="entrar-sorteo" style="text-decoration: none">Haz click aqui para participar</a>' +
-                '</button>'
+                '</button>' +
             '</section>';
 
-        $("#sorteo").append(paco);
-        c=c+1;
+        $("#sorteos").append(item);
     });
 });
