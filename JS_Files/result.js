99212ce4c0930b6b7c7594281996e6682500fa95
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2022-03-25 00:26:56 +0000

    Cambio de medidas en diseño de tablet y añadida carga por archivo JSON en slider_result

---
+++ JS_Files/result.js
@@ -0,0 +1,27 @@
+
+$.getJSON("/JSON_Files/result.json", function(data) {
+    $.each(data.result_top_images, function(key, value) {
+
+        let top_item;
+
+        top_item = '<article>' +
+            '<img src="' + value.img + '" alt="">' +
+            '<label>' + value.name + '</label>'
+            '</article>';
+
+        $("#top").append(top_item);
+
+    });
+
+    $.each(data.result_bot_images, function(key, value) {
+
+        let bot_item;
+
+        bot_item = '<img src="' + value.img + '" alt="">' +
+            '<label>' + value.name + '</label>'
+        '</article>';
+
+        $("#bot").append(bot_item);
+
+    });
+});
